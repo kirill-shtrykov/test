86567f18e8fee53a668e821ce143617e4e0232a1
Kirill Shtrykov <kirill@shtrykov.com>
2024-04-26 14:25:46 +0200

    ci: Test `version.sh`

--- version.sh
+++ version.sh
@@ -23,17 +23,17 @@ fi
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "Updating $CURRENT_VERSION to $NEW_TAG"
 
-#get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+# #get current hash and see if it already has a tag
+# GIT_COMMIT=`git rev-parse HEAD`
+# NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
-#only tag if no tag already
-if [ -z "$NEEDS_TAG" ]; then
-  git tag -a "${NEW_TAG}" -m "${CHANGES}"
-  git push --tags
-  git push
-else
-  echo "Already a tag on this commit" && exit 1
-fi
+# #only tag if no tag already
+# if [ -z "$NEEDS_TAG" ]; then
+#   git tag -a "${NEW_TAG}" -m "${CHANGES}"
+#   git push --tags
+#   git push
+# else
+#   echo "Already a tag on this commit" && exit 1
+# fi
 
-exit 0
+# exit 0
