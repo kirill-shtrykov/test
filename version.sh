3e2969700f216e10f411729ef362d259299f47ac
Kirill Shtrykov <kirill@shtrykov.com>
2024-04-26 14:18:23 +0200

    feat: Update `version.sh`

--- version.sh
+++ version.sh
@@ -8,4 +8,32 @@ VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-git log --pretty=oneline HEAD...${CURRENT_VERSION}
+CHANGES=$(git log --pretty=oneline HEAD...${CURRENT_VERSION} | awk '{ $1=""; print "-" $0 }')
+echo "Changes:"
+echo ${CHANGES}
+if [[ $CHANGES =~ "feat" ]]
+then
+    VNUM2=$((VNUM2+1))
+    VNUM3=0
+else
+    VNUM3=$((VNUM3+1))
+fi
+
+#create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+echo "Updating $CURRENT_VERSION to $NEW_TAG"
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+#only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+  git tag -a "${NEW_TAG}" -m "${CHANGES}"
+  git push --tags
+  git push
+else
+  echo "Already a tag on this commit" && exit 1
+fi
+
+exit 0
